74546d543ec476c14c33dd778866a691398bae86
saya0118 <chemi0802@gmail.com>
2021-11-26 16:27:40 -0800

    added h1

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -3,7 +3,15 @@ import React from 'react'
 function Skills (){
     return(
         <div className="skills">
-            
+            <ul>
+                <li>HTM5</li>
+                <li>SCC/SASS</li>
+                <li>JavaScript</li>
+                <li>React.js</li>
+                <li>Redux</li>
+                <li>TypeScript</li>
+                <li>GitHub</li>
+            </ul>
         </div>
     )
 }
--- src/components/Works.js
+++ src/components/Works.js
@@ -3,7 +3,8 @@ import React from "react"
 function Works (){
     return(
         <div className="works">
-
+            <h1>WORKS</h1>
+            <p className="underline"></p>
         </div>
     )
 }
