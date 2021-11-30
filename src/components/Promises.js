7e7c1324576a2e9ab320e141c3106d6973b11571
saya0118 <chemi0802@gmail.com>
2021-11-29 19:59:25 -0800

    organize promise

--- src/components/Promises.js
+++ src/components/Promises.js
@@ -5,7 +5,30 @@ function Promises () {
         <div className="promises">
             <div className="section-title">
                 <h1>PROMISES</h1>
+                <p>I promise ...</p>
             </div>
+            <ul>
+                <li>
+                    <img src=""/>
+                    <h3></h3>
+                </li>
+                <li>
+                    <img src=""/>
+                    <h3></h3>
+                </li>
+                <li>
+                    <img src=""/>
+                    <h3></h3>
+                </li>
+                <li>
+                    <img src=""/>
+                    <h3></h3>
+                </li>
+                <li>
+                    <img src=""/>
+                    <h3></h3>
+                </li>
+            </ul>
 
         </div>
     )
