af045194fc0c80ce75354c2dbc10354cbb5b78d3
saya0118 <chemi0802@gmail.com>
2021-12-11 19:48:13 -0800

    created promise

--- src/components/Promises.js
+++ src/components/Promises.js
@@ -1,4 +1,5 @@
 import React from "react"
+import 'semantic-ui-css/semantic.min.css'
 
 function Promises () {
     return(
@@ -7,26 +8,26 @@ function Promises () {
                 <h1>PROMISES</h1>
             </div>
             <p>I promise ...</p>
-            <ul>
-                <li>
-                    <img src=""/>
-                    <h3></h3>
+            <ul className="ui container five column grid">
+                <li className="ui column">
+                    <i className="ui massive fire icon"></i>
+                    <h3>Passion</h3>
                 </li>
-                <li>
-                    <img src=""/>
-                    <h3></h3>
+                <li className="ui column">
+                    <i className="ui massive calendar alternate outline icon"></i>
+                    <h3>Time</h3>
                 </li>
-                <li>
-                    <img src=""/>
-                    <h3></h3>
+                <li className="ui column">
+                    <i className="ui massive comment alternate outline icon"></i>
+                    <h3>Suggestion</h3>
                 </li>
-                <li>
-                    <img src=""/>
-                    <h3></h3>
+                <li className="ui column">
+                    <i className="ui massive handshake outline icon"></i>
+                    <h3>Cooperate</h3>
                 </li>
-                <li>
-                    <img src=""/>
-                    <h3></h3>
+                <li className="ui column">
+                    <i className="ui massive recycle icon"></i>
+                    <h3>PDCA</h3>
                 </li>
             </ul>
 
